71acf3d49e63c0df8130667d61dc5f419e5a8f6a
Anna Groffsky <groffsky@umich.edu>
2019-12-11 01:19:50 -0500

    DONE WITH THE VISUALS

--- foursquareprice.py
+++ foursquareprice.py
@@ -10,34 +10,34 @@ root = os.path.dirname(os.path.abspath(__file__)) + os.sep
 file_name = root + 'ratings.sqlite'
 conn = sqlite3.connect(file_name)
 cur =  conn.cursor()
-cur.execute("SELECT restaurants, avgrating FROM FoursquareRating")
+cur.execute("SELECT restaurants, priceTier FROM FoursquarePrice")
 #make a dict 
-foursquare_ratings = {}
+foursquare_price = {}
 for row in cur: 
-    foursquare_ratings[row[0]] =  row[1]
+    foursquare_price[row[0]] =  row[1]
 
 #name a text file and write the results to that 
-with open('foursquare_ratings.text', 'w') as outfile: 
-    json.dump(foursquare_ratings, outfile)
+with open('foursquare_price.text', 'w') as outfile: 
+    json.dump(foursquare_price, outfile)
 
 #assign x and y
-x_restaurants = foursquare_ratings.keys()
-y_ratings = foursquare_ratings.values()
+x_restaurants = foursquare_price.keys()
+y_ratings = foursquare_price.values()
 
 #formatting
 index = np.arange(len(x_restaurants))
 plt.tight_layout()
 plt.gcf().subplots_adjust(bottom=0.25)
-plt.bar(index, y_ratings, align="center", width=0.5, color=["indigo"])
+plt.bar(index, y_ratings, align="center", width=0.5, color=["green"])
 plt.xticks(index, x_restaurants, rotation="vertical", fontsize=6)
 
 #naming axes 
 plt.xlabel("Restaurants")
-plt.ylabel("Average Foursquare Rating")
-plt.title("Foursquare Ratings of AA Restaurants ")
+plt.ylabel("Price Tier")
+plt.title("Price Tier Rating of AA Restaurants (from Foursquare)")
 
 #where to save the figure 
-plt.savefig("Foursquare_ratings.png")
+plt.savefig("foursquare_price.png")
 
 plt.show()
 
--- yelprating.py
+++ yelprating.py
@@ -1,15 +1,17 @@
 import sqlite3
 import json 
+import os
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
 #connect to database
-conn = sqlite3.connect("/Users/AnnaGroffsky/Desktop/ratings.sqlite")
-cur = conn.cursor()
-
+root = os.path.dirname(os.path.abspath(__file__)) + os.sep
+file_name = root + 'ratings.sqlite'
+conn = sqlite3.connect(file_name)
+cur =  conn.cursor()
 cur.execute("SELECT restaurants, avgrating FROM YelpRating")
-#make a dict so {rating number: number of restaurants}
+#make a dict 
 yelp_ratings = {}
 for row in cur: 
     yelp_ratings[row[0]] = row[1]
@@ -19,16 +21,16 @@ for row in cur:
 with open('yelp_ratings.text', 'w') as outfile: 
     json.dump(yelp_ratings, outfile)
 
-#assign x axis to rating number and y to number of restaurants 
+#assign x and y
 x_restaurants = yelp_ratings.keys()
 y_ratings = yelp_ratings.values()
 
 #formatting
 index = np.arange(len(x_restaurants))
 plt.tight_layout()
-plt.gcf().subplots_adjust(bottom=0.15)
+plt.gcf().subplots_adjust(bottom=0.25)
 plt.bar(index, y_ratings, align="center", width=0.5, color=["red"])
-plt.xticks(index, x_restaurants, rotation="vertical", fontsize=3)
+plt.xticks(index, x_restaurants, rotation="vertical", fontsize=6)
 
 #naming axes 
 plt.xlabel("Restaurants")
